0467c7523af16e24bb116bef8ca06e88cd3cc2af
oguran <oguran29@gmail.com>
2025-08-21 00:01:27 +0900

    Add world files for intern.

--- raspimouse_gazebo/launch/raspimouse_with_intern_world_01_for_ros1_bridge.launch.py
+++ raspimouse_gazebo/launch/raspimouse_with_intern_world_01_for_ros1_bridge.launch.py
@@ -9,23 +9,31 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import SetParameter
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    world_file = os.path.join(
-        get_package_share_directory('raspimouse_gazebo'),
-        'worlds',
-        'intern_world_01.sdf')
-    world_launch = IncludeLaunchDescription(
+    intern_world_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             get_package_share_directory('raspimouse_gazebo'),
-            '/launch/raspimouse_with_emptyworld.launch.py']),
+            '/launch/raspimouse_with_intern_world_01.launch.py'
+        ]),
         launch_arguments={
-            'world_name': world_file,
-            }.items()
+            'lidar': 'urg',
+            'use_rgb_camera': 'true'
+        }.items()
     )
 
+    # リマッピング用のリレーノード
+    relay_scan = Node(
+        package='topic_tools',
+        executable='relay',
+        arguments=['/scan', '/bridge/scan'],
+        name='scan_relay'
+    )
+   
     return LaunchDescription([
         SetParameter(name='use_sim_time', value=True),
-        world_launch
+        intern_world_launch,
+        relay_scan,
     ])
